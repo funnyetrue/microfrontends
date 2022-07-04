096017ade15124b77ef9d4891d85d029993d2f39
funnyetrue <funnyetrue@gmail.com>
2022-07-04 14:02:41 -0400

    updated test for container build

--- packages/container/src/components/TestComponent.js
+++ packages/container/src/components/TestComponent.js
@@ -1,1 +1,3 @@
-//test component for deploy-container workflow, updating to test with default regionin yml file
+//test component for deploy-container workflow
+//updating to test with default regionin yml file
+//updating to test aws-distribution-id and invalidation step
