310282f4beda849ce80bca0b4d7358cdedade136
funnyetrue <funnyetrue@gmail.com>
2022-07-14 08:42:55 -0400

    fixed generateClassName issue

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { StylesProvider, generateClassName } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
 
